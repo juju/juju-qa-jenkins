72b3e02dc623e418f4a4886e9e5153a06901c316
Simon Richardson <stickupkid@gmail.com>
2023-02-23 12:37:44 +0000

    Ensure we have the right dependencies

--- jobs/ci-run/scripts/snippet_make-musl-cross-build.sh
+++ jobs/ci-run/scripts/snippet_make-musl-cross-build.sh
@@ -6,8 +6,10 @@ sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
 sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 sudo docker run --mount "type=bind,source=$JUJU_SRC_PATH,target=/juju" "multiarch/ubuntu-core:{arch}-focal" /bin/bash -c '
+DEBIAN_FRONTEND=noninteractive
+TZ=Europe/London
 apt-get update
-apt-get install sudo make -y
+apt-get install sudo build-essential golang-go git curl wget -y
 cd /juju
 make -j`nproc` MUSL_PRECOMPILED=0 musl-install
 '
