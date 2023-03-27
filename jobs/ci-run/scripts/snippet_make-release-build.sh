2f15e321e075667ec1f99464a586fcad8dab36a5
Simon Richardson <stickupkid@gmail.com>
2023-03-27 10:01:27 +0100

    Set GOOS and GOARCH correctly
    We need to set these up correctly so that dqlite and musl dependencies
    are correctly picked up when downloading.
    
    This should fix the CI build issues.

--- jobs/ci-run/scripts/snippet_make-release-build.sh
+++ jobs/ci-run/scripts/snippet_make-release-build.sh
@@ -9,4 +9,11 @@ echo AGENT_PACKAGE_PLATFORMS=${{AGENT_PACKAGE_PLATFORMS}}
 echo BUILD_TAGS=${{BUILD_TAGS:-}}
 
 cd ${{JUJU_SRC_PATH}}
-make -j`nproc` go-build BUILD_TAGS="${{BUILD_TAGS:-}}"
+build_type=$(grep "go-agent-build-no-cgo" Makefile >/dev/null && echo "no-cgo")
+if [ $build_type = "no-cgo" ]; then
+    GOOS=$(echo $AGENT_PACKAGE_PLATFORMS | cut -d/ -f 1) \
+    GOARCH=$(echo $AGENT_PACKAGE_PLATFORMS | cut -d/ -f 2) \
+        make -j`nproc` go-build BUILD_TAGS="${{BUILD_TAGS:-}}"
+else
+    make -j`nproc` go-build BUILD_TAGS="${{BUILD_TAGS:-}}"
+fi
