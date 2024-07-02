8dd8b230ba472cf4c0e6024a2e57a628861135c2
Alastair Flynn <af@aflynn.uk>
2024-07-02 15:05:52 +0200

    fix: GO111MODULE broken with go 1.22 update
    Go no longer support go get outside of a module with the GO111MODULE
    enviromental var: https://tip.golang.org/doc/go1.22. Change the uses of
    this to go install instead. A version for the modules is required since
    it would normally use the go.mod one.

--- jobs/github/scripts/run-snippet-lxd.sh
+++ jobs/github/scripts/run-snippet-lxd.sh
@@ -282,7 +282,7 @@ lxc exec "${{CONTAINER_NAME}}" -- sudo -u ubuntu bash <<"EOT"
     if [ -f "${{WORKSPACE}}/go-unittest.out" ]; then
         # This will be used to generate reports for jenkins assuming there is
         # a file "go-unittest.out" in the WORKSPACE.
-        GO111MODULE=off go get -v github.com/tebeka/go2xunit
+        go install -v github.com/tebeka/go2xunit@latest
         "${{GOPATH}}/bin/go2xunit" -fail -input "${{WORKSPACE}}/go-unittest.out" -output "${{WORKSPACE}}/tests.xml"
     fi
 
