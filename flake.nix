03390cf0c2be6705b0cae0f56a5d1cf259430ee6
Harry Pidcock <harry.pidcock@canonical.com>
2024-07-05 10:17:03 +1000

    build: add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "juju qa jenkins job builder shell";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+    let
+      forAllSystems = inputs.nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (system: {
+        default =
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          pkgs.mkShell {
+            name = "juju-qa-jenkins";
+            # Enable experimental features without having to specify the argument
+            NIX_CONFIG = "experimental-features = nix-command flakes";
+            nativeBuildInputs = with pkgs; [
+              gnumake
+              zsh
+              python3
+              shellcheck
+            ];
+            shellHook = ''
+              exec zsh
+            '';
+          };
+      });
+    };
+}
